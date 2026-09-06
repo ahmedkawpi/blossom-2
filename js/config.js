515a4186f8a42e4c12a4105713131c47165b2255
ahmedkawpi <ahmedkapwira@gmail.com>
2026-09-06 05:16:32 +0100

    Fix Supabase connection

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const SUPABASE_CONFIG = {
     url: "https://nqvxnjmkkvvvyqwstkaa.supabase.co",
-    publishableKey: "sb_publishable_..."
+    publishableKey: "sb_publishable_9nmpN3ToD6_MlpiqN7wN4w_ma40PzBj"
 };
 const STORE_CONFIG = {
     name: "man clothes",
